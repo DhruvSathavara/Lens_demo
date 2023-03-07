e6b2fc46434a6c9622b0ed846f725037bc19e9a9
DhruvSathavara <dhruvsathavara7228@gmail.com>
2023-03-08 01:05:34 +0530

    search-profile

--- src/App.js
+++ src/App.js
@@ -1,58 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Web3Modal from "web3modal";
-import { ethers, Contract, providers, Signer } from 'ethers';
-import { generateChallenge } from './lens/authentication';
-import { authenticate } from './lens/authentication';
-import UploadModal from './componant/createPost-Modal';
-import { LensAuthContext } from './context/LensContext';
-import ProfileCreate from './lens/profile/create-profile-modal';
-import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
-import Profile from './lens/profile/ProfileModal';
-import DisplayPublications from './lens/getPost/DisplayPost';
-import ReportModal from './lens/ReportPublication/report-modal';
-import ReporrtModal from './lens/ReportPublication/report-modal';
-
+import React from 'react';
+import Header from "./componant/Header"
 function App() {
-
-  const lensAuthContext = React.useContext(LensAuthContext);
-  const { profile, login, setUpdate, update } = lensAuthContext;
-  // console.log(profile, 'profile');
-
 
   return (
     <>
       <div>
         {
-          <div>
-            <Button className='m-2' style={{ background: '#488E72', color: 'white', textTransform: 'capitalize' }} onClick={login}>
-              Login
-            </Button>
-            <Link to='/Createprofile'>
-              <Button className='m-2' style={{ background: '#488E72', color: 'white', textTransform: 'capitalize' }} >
-                Create Profile
-              </Button>
-            </Link>
-            <Link to='/profile'>
-              <Button className='m-2' style={{ background: 'rgb(35 51 44)',right:"0", color: 'white', textTransform: 'capitalize' }} >
-                Profile
-              </Button>
-            </Link>
-            <Link to='/publications'>
-              <Button className='m-2' style={{ background: 'rgb(35 51 44)',right:"0", color: 'white', textTransform: 'capitalize' }} >
-                Publications
-              </Button>
-            </Link>
-          </div>
-        }<br /><br />
-        <UploadModal />
-        {/* <ProfileCreate /> */}
-       
+          <Header></Header>
+        }
       </div>
-      <div>
-      
-      </div>
-   
     </>
   );
 }
